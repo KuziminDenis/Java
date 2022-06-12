2836d48a710fd21aaf6d08265df449720c7116e8
Denni <teamdenis@gmail.com>
2022-06-12 15:21:56 +0300

    EvenOdd

---
+++ src/stepIt/Homework/EvenOdd.java
@@ -0,0 +1,18 @@
+package stepIt.Homework;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        while(input.hasNextInt()){
+            int x = input.nextInt();
+            if(x%2 == 0)
+                System.out.println(x + " - четное");
+           else
+                System.out.println(x + " - нечетное");
+            System.out.print("Введите число: ");
+        }
+    }
+}
