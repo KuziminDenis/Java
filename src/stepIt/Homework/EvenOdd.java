f2af87e7555dc653ead55a21aace0ef1859f45b8
Denni <teamdenis@gmail.com>
2022-06-13 19:53:14 +0300

    evenodd

--- src/stepIt/Homework/EvenOdd.java
+++ src/stepIt/Homework/EvenOdd.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class EvenOdd {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Введите число: ");
+        System.out.print("Введите число, выход из программы ввод буквенного значения: ");
         while(input.hasNextInt()){
             int x = input.nextInt();
             if(x%2 == 0)
